3ea4a5ad918f72d736803f360e643da8870ef740
xaviertnc <xavier.tnc@gmail.com>
2021-02-11 08:17:40 +0200

    FH v0.0.6 - FT - Add default taxonomy page template.

---
+++ taxonomy.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * @package FUNDHUB
+ * @subpackage Templates
+ * @version 1.0.0
+ */
+
+get_header(); ?>
+
+<!-- SITE CONTENT -->
+<section id="site-content">
+
+<?php get_template_part( 'templates/part/post-header' ); ?>
+
+<!-- Page Content -->
+<main id="page-content" role="main">
+
+<!-- content container -->
+<div class="container clr" role="presentation">
+
+<!-- content left -->
+<div class="content-left" role="presentation">
+
+<?php if ( $taxonomy = get_queried_object() ) : ?>
+<h3><?= $taxonomy->name ?></h3>
+<div class="archive-meta"><?= $taxonomy->description ?></div>
+<?php endif; ?>
+
+</div>
+<!-- /content left -->
+
+<!-- content right -->
+<?php dynamic_sidebar('sidebar-main'); ?>
+<!-- /content right -->
+
+</div>
+<!-- /content container -->
+
+</main>
+<!-- /Page Content -->
+
+<?php // get_template_part( 'templates/part/page-footer' ); ?>
+
+</section>
+<!-- /SITE CONTENT -->
+
+<?php get_footer(); ?>
