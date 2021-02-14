8f65ccb9698e4e95efa95df71627f82d891ed47b
xaviertnc <xavier.tnc@gmail.com>
2021-02-14 12:51:10 +0200

    FH v0.1.18 - FT - tools-reset-wp: Refactor into FH_Reset_WP class + Add Delete ALL UPLOADS + Confirm Delete!

--- includes/admin/fh_admin_tools_wp-reset.php
+++ includes/admin/fh_admin_tools_wp-reset.php
@@ -1,122 +1,145 @@
 <?php
 
-function fh_action_wp_reset()
-{
-	global $current_user, $wpdb;
+class FH_Reset_WP {
 
-  check_admin_referer( 'fh_nonce' );
 
-  if ( ! current_user_can( 'administrator' ) )
+  function __construct()
   {
-    return false;
+    add_action( 'admin_post_fh_wp_reset', array( $this, 'reset' ) );
   }
 
-  // make sure the function is available to us
-  if ( ! function_exists( 'wp_install' ) )
+
+  function reset()
   {
-    require ABSPATH . '/wp-admin/includes/upgrade.php';
-  }
+  	global $current_user, $wpdb;
 
-  $restore_theme = ! empty( $_POST[ 'restore_theme' ] );
+    check_admin_referer( 'fh_nonce' );
 
-  // save values that need to be restored after reset
-  $blogname = get_option( 'blogname' );
-  $show_on_front = get_option( 'show_on_front' );
-  $page_on_front = get_option( 'page_on_front' );
-  $page_for_posts = get_option( 'page_for_posts' );
-  $blog_description = get_option( 'blogdescription' );
-  $blog_public = get_option( 'blog_public' ); // Discourage search engines (1 or 0)
-  $wplang = get_option( 'WPLANG', 'en_ZA' );
-  $siteurl = get_option( 'siteurl' );
-  $home = get_option( 'home' );
+    if ( ! current_user_can( 'administrator' ) )
+    {
+      return false;
+    }
 
-  $active_plugins = get_option( 'active_plugins' );
-  $active_theme = wp_get_theme();
+    // make sure the function is available to us
+    if ( ! function_exists( 'wp_install' ) )
+    {
+      require ABSPATH . '/wp-admin/includes/upgrade.php';
+    }
 
-  // delete custom tables with WP's prefix
-  $prefix = str_replace( '_', '\_', $wpdb->prefix );
-  $tables = $wpdb->get_col( "SHOW TABLES LIKE '{$prefix}%'" );
-  foreach ( $tables as $table )
-  {
-    $wpdb->query( "DROP TABLE $table" );
-  }
+    $do_clear_content   = ! empty( $_POST[ 'clear_content' ] );
+    $do_restore_plugins = ! empty( $_POST[ 'restore_plugins' ] );
+    $do_restore_theme   = ! empty( $_POST[ 'restore_theme' ] );
+    $do_delete_uploads  = ! empty( $_POST[ 'delete_uploads' ] );
+    $confirmed_delete   = ! empty( $_POST[ 'confirm_delete_uploads' ] );
 
-  $result = @wp_install(
-    $restore_theme ? $blogname : 'Vanilla Wordpress',
-    $current_user->user_login,
-  	$current_user->user_email,
-    $blog_public,
-    '',
-    md5(rand()),
-    $wplang
-  );
-  $user_id = $result[ 'user_id' ];
+    // save values that need to be restored after reset
+    //$show_on_front = get_option( 'show_on_front' );
+    //$page_on_front = get_option( 'page_on_front' );
+    //$page_for_posts = get_option( 'page_for_posts' );
+    $blogname = get_option( 'blogname' );
+    $blog_description = get_option( 'blogdescription' );
+    $blog_public = get_option( 'blog_public' ); // Discourage search engines (1 or 0)
+    $wplang = get_option( 'WPLANG', 'en_ZA' );
+    $siteurl = get_option( 'siteurl' );
+    $home = get_option( 'home' );
 
-  // restore user pass
-  $sql = "UPDATE {$wpdb->users} SET user_pass = %s, user_activation_key = ''" .
-   ' WHERE ID = %d LIMIT 1';
+    $active_plugins = get_option( 'active_plugins' );
+    $active_theme = wp_get_theme();
 
-  $query = $wpdb->prepare( $sql, array( $current_user->user_pass, $user_id ) );
-  $wpdb->query( $query );
-
-  // restore rest of the settings
-  update_option( 'siteurl', $siteurl, true );
-  update_option( 'home', $home, true );
-
-  // remove password nag
-  if ( get_user_meta( $user_id, 'default_password_nag' ) )
-  {
-    update_user_meta( $user_id, 'default_password_nag', false );
-  }
-  if ( get_user_meta( $user_id, $wpdb->prefix . 'default_password_nag' ) )
-  {
-    update_user_meta( $user_id, $wpdb->prefix . 'default_password_nag', false );
-  }
-
-  // delete all default pages, posts & comments
-  if ( ! empty( $_POST[ 'clear_content' ] ) )
-  {
-    $tables = array( $wpdb->postmeta, $wpdb->posts, $wpdb->comments );
+    // delete custom tables with WP's prefix
+    $prefix = str_replace( '_', '\_', $wpdb->prefix );
+    $tables = $wpdb->get_col( "SHOW TABLES LIKE '{$prefix}%'" );
     foreach ( $tables as $table )
     {
-      $wpdb->query( "TRUNCATE TABLE $table" );
+      $wpdb->query( "DROP TABLE $table" );
     }
-  }
 
-  // reactivate all plugins
-  if ( ! empty( $_POST[ 'restore_plugins' ] ) )
-  {
-    foreach ( $active_plugins as $plugin_file )
+    $result = @wp_install(
+      $do_restore_theme ? $blogname : 'Vanilla Wordpress',
+      $current_user->user_login,
+    	$current_user->user_email,
+      $blog_public,
+      '',
+      md5(rand()),
+      $wplang
+    );
+    $user_id = $result[ 'user_id' ];
+
+    // restore user pass
+    $sql = "UPDATE {$wpdb->users} SET user_pass = %s, user_activation_key = ''" .
+     ' WHERE ID = %d LIMIT 1';
+
+    $query = $wpdb->prepare( $sql, array( $current_user->user_pass, $user_id ) );
+    $wpdb->query( $query );
+
+    // restore rest of the settings
+    update_option( 'siteurl', $siteurl, true );
+    update_option( 'home', $home, true );
+
+    // remove password nag
+    if ( get_user_meta( $user_id, 'default_password_nag' ) )
     {
-      activate_plugin( $plugin_file );
+      update_user_meta( $user_id, 'default_password_nag', false );
     }
+    if ( get_user_meta( $user_id, $wpdb->prefix . 'default_password_nag' ) )
+    {
+      update_user_meta( $user_id, $wpdb->prefix . 'default_password_nag', false );
+    }
+
+    // Delete all posts, post metas, comments & comment metas
+    if ( $do_clear_content )
+    {
+      $tables = array( $wpdb->postmeta, $wpdb->posts,
+        $wpdb->comments, $wpdb->commentmeta );
+      foreach ( $tables as $table )
+      {
+        $wpdb->query( "TRUNCATE TABLE $table" );
+      }
+    }
+
+    if ( $do_delete_uploads and $confirmed_delete )
+    {
+      $uploads_info = wp_get_upload_dir();
+      $base_dir = uploads_info[ 'basedir' ];
+      FH_Lib::$delete_count = 0;
+      FH_Lib::delete_dir( $base_dir, $base_dir );
+    }
+
+    if ( $do_restore_plugins )
+    {
+      foreach ( $active_plugins as $plugin_file )
+      {
+        activate_plugin( $plugin_file );
+      }
+    }
+
+    if ( $do_restore_theme )
+    {
+      switch_theme( $active_theme->get_stylesheet() );
+      // set theme default options
+      //update_option( 'show_on_front', $show_on_front, true );
+      //update_option( 'page_for_posts', $page_for_posts, true );
+      //update_option( 'page_on_front', $page_on_front, true );
+      update_option( 'blogdescription', $blog_description, true );
+      update_option( 'permalink_structure', '/%category%/%postname%/', true );
+      update_option( 'uploads_use_yearmonth_folders', 0, true );
+      update_option( 'wp_page_for_privacy_policy', 0, true );
+      update_option( 'upload_path', 'media', true );
+      update_option( 'thumbnail_crop', 0, true );
+      update_option( 'ping_sites', '', true );
+    }
+
+    // log out and log in
+    wp_clear_auth_cookie();
+    wp_set_auth_cookie( $user_id );
+
+    wp_redirect( admin_url() . '?wp-reset=success' );
+
+    exit;
+
   }
-
-  // reactivate theme
-  if ( $restore_theme )
-  {
-    switch_theme( $active_theme->get_stylesheet() );
-    // set theme default options
-    update_option( 'blogdescription', $blog_description, true );
-    update_option( 'permalink_structure', '/%category%/%postname%/', true );
-    update_option( 'uploads_use_yearmonth_folders', 0, true );
-    update_option( 'wp_page_for_privacy_policy', 0, true );
-    update_option( 'show_on_front', $show_on_front, true );
-    update_option( 'page_for_posts', $page_for_posts, true );
-    update_option( 'page_on_front', $page_on_front, true );
-    update_option( 'upload_path', 'media', true );
-    update_option( 'thumbnail_crop', 0, true );
-    update_option( 'ping_sites', '', true );
-  }
-
-  // log out and log in
-  wp_clear_auth_cookie();
-  wp_set_auth_cookie( $user_id );
-
-  wp_redirect( admin_url() . '?wp-reset=success' );
-
-  exit;
 
 }
 
-add_action( 'admin_post_fh_wp_reset', 'fh_action_wp_reset' );
+
+new FH_Reset_WP();
--- templates/template-tools-page.php
+++ templates/template-tools-page.php
@@ -3,11 +3,15 @@
 <hr class="wp-header-end">
 <form action="<?=esc_url( admin_url( 'admin-post.php' ) )?>" method="post">
 	<h2>Reset Wordpress</h2>
-  <p><label><input type="checkbox" name="clear_content" checked> Clear ALL Posts, Pages & Comments</label></p>
-  <p><label><input type="checkbox" name="restore_plugins" checked> Restore Currently Active Plugins</label></p>
+  <p><label><input type="checkbox" name="clear_content" checked> Delete ALL Posts, Pages & Comments</label></p>
+  <p><label><input type="checkbox" name="restore_plugins" checked> Reactivate Currently Active Plugins</label></p>
   <p><label><input type="checkbox" name="restore_theme" checked> Restore Current Theme</label></p>
+  <p><label style="color:crimson"><input type="checkbox" id="delete_uploads"
+    name="delete_uploads" onchange="fh_confirm_delete_uploads(event)">
+      Delete ALL Uploads</label></p>
   <?=$nonce_field?>
   <input type="hidden" name="action" value="fh_wp_reset">
+  <input type="hidden" id="confirm_delete" name="confirm_delete_uploads" value="0">
   <?=submit_button( 'Reset Wordpress' )?>
 </form>
 <br>
@@ -22,4 +26,18 @@
     <input type="hidden" name="action" value="fh_import_data">
     <?=submit_button( 'Import Data' )?>
 </form>
+<script>
+  var elDelUploads = document.getElementById('delete_uploads');
+  var elConfirmDelete = document.getElementById('confirm_delete');
+  function fh_confirm_delete_uploads( event ) {
+    if ( ! elDelUploads.checked ) { elConfirmDelete.value = 0; return true; }
+    event.preventDefault();
+    event.stopImmediatePropagation();
+    if( confirm('Delete ALL files in UPLOADS... Are you sure!?') ) {
+      elConfirmDelete.value = 1;
+    } else {
+      elDelUploads.checked = false;
+    }
+  }
+</script>
 </div>
