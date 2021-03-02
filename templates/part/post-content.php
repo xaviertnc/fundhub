15735e344e988bcfc95657c0474d7cd8be603f67
xaviertnc <xavier.tnc@gmail.com>
2021-03-02 18:59:40 +0200

    FH v0.2.4 - DEV - part/post-content: Add ID to website link for Google Tag Manager tracking.

--- templates/part/post-content.php
+++ templates/part/post-content.php
@@ -1,19 +1,20 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-  <!-- Post Header -->
-  <header class="post-header">
-    <div class="content-left" role="presentation">
-      <?php the_post_thumbnail( 'full', ['class' => 'img-featured' ] ); ?>
+
+<!-- Post Header -->
+<header class="post-header">
+  <div class="content-left" role="presentation">
+    <?php the_post_thumbnail( 'full', ['class' => 'img-featured' ] ); ?>
+
+  </div>
+  <div class="content-right clr" role="presentation">
+    <div class="cta-goto-website pull-right">
+      <label>Asset Manager's Website</label>
+      <a id="asset_managers_website" href="<?=get_post_meta( $post->ID, 'Website', true )
+        ?>" class="link-button" target="_blank">Visit <?=$post->post_title?></a>
     </div>
-    <div class="content-right clr" role="presentation">
-      <div class="cta-goto-website pull-right">
-        <label>Asset Manager's Website</label>
-        <a href="<?=get_post_meta( $post->ID, 'Website', true )?>" class="link-button">
-          Visit <?=$post->post_title?>
-        </a>
-      </div>
-    </div>
-  </header>
-  <!-- /Post Header -->
+  </div>
+</header>
+<!-- /Post Header -->
 <?php
   $more_link_text = null; $strip_teaser = null;
   $content = get_the_content( $more_link_text, $strip_teaser );
